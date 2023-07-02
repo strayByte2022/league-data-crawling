8637bb92cb35ee5d433555ec26a26c145a15eeeb
l1nhx <linhhandsom@gmail.com>
2023-07-02 14:55:30 +0700

    player info test file

--- player_info.py
+++ player_info.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
 api_keys = "RGAPI-9d4ba430-98df-48ca-b249-f7958883df94"
 url_api = "https://vn2.api.riotgames.com/lol/summoner/v4/summoners/by-name/TinChuanChuaAnh"+'?api_key='+api_keys
@@ -11,5 +8,4 @@ player_info = status.json()
 
 
 print(status)
-print(player_info)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
